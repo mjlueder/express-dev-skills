47bd74c6e9241ca9f973ac503ecc4f941f1c2075
Mary Lueder <mlueder@gmail.com>
2023-01-06 19:45:36 -0500

    add update functionality, back buttons, tweak styling

--- controllers/skills.js
+++ controllers/skills.js
@@ -67,6 +67,17 @@ function edit(req, res){
   })
 }
 
+function update(req, res){
+  Skill.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(skill => {
+    res.redirect(`/skills/${skill._id}`)
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
   newSkill as new,
@@ -74,4 +85,5 @@ export {
   show,
   deleteTodo as delete,
   edit,
+  update
 }
